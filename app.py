e8d81402c63cbba5472b78dbc1f36f094d1e0e31
crlsprof4005647-hue <crlsprof.4005647@gmail.com>
2026-09-06 20:44:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.title("🚀 Meu App Conectado ao Google Sheets!")
 conn = st.connection("gsheets", type=GSheetsConnection)
 
 # COLE O LINK DA SUA PLANILHA AQUI DENTRO DAS ASPAS
-URL_PLANILHA = "COLE_AQUI_O_LINK_DA_SUA_PLANILHA"
+URL_PLANILHA = "https://docs.google.com/spreadsheets/d/1rkpKc13ezgVPHM7CJYmFsO10EP_dqXO4Pkf6AYPrnqU/edit?usp=sharing"
 
 # Lê a planilha forçando a atualização na mesma hora (ttl=0)
 df = conn.read(spreadsheet=URL_PLANILHA, ttl=0)
